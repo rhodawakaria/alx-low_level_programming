26ec2179d133099f90f8afd7bd3b5f8321b58ba1
Rhoda Wakaria <wakariarhoda@gmail.com>
2022-03-22 03:50:22 -0700

    initial commit

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_array - print an array
+ *
+ * @a: the array to print
+ * @n: array's length
+ *
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
+		if (i < n - 1)
+		{
+			printf(", ");
+		}
+	}
+
+	printf("\n");
+}
