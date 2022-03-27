af104d9275817d3eb8c734f63c27160a2eae40f6
Rhoda Wakaria <wakariarhoda@gmail.com>
2022-03-27 09:18:04 -0700

    Correct output

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -12,33 +12,39 @@ void print_times_table(int n)
 {
 	if (n >= 0 && n <= 15)
 	{
-		int i;
-
-		for (i = 0; i <= n; i++)
+		if (n == 0)
 		{
-			int j;
+			printf("0");
+		}
 
-			for (j = 0; j <= n; j++)
+		else
+		{
+			int i;
+
+			for (i = 0; i <= n; i++)
 			{
-				int product = i * j;
-				int width = n * n;
-				int digits = width > 0 && width < 100 ? 2 : 3;
+				int j;
 
-				if (j == 0)
+				for (j = 0; j <= n; j++)
 				{
-					printf("%d, ", product);
-				}
+					int product = i * j;
+					int digits = 3;
 
-				else if (j < n)
-				{
-					printf("%*d, ", digits, product);
+					if (j == 0)
+					{
+						printf("%d, ", product);
+					}
+					else if (j < n)
+					{
+						printf("%*d, ", digits, product);
+					}
+					else
+					{
+						printf("%*d", digits, product);
+					}
 				}
-				else
-				{
-					printf("%*d ", digits, product);
-				}
+				putchar('\n');
 			}
-			putchar('\n');
 		}
 	}
 
