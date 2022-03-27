d45ea973b5a8545f9efd7e04f25c47e37c4671c4
Rhoda Wakaria <wakariarhoda@gmail.com>
2022-03-27 02:31:00 -0700

    Initial commit

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - adds two integers and returns the result
+ * @a: an integer to add to b
+ * @b: an integer to add to a
+ * Return: Always 0
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
